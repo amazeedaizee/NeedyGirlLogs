f4254161f320d69616fd1eaf8c93430dd73183a0
amazeedaizee <amazeedaizee@WINDSTORM>
2024-01-30 23:54:08 -0500

    Add project files.

---
+++ Initializer.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+
+namespace PlaythroughLogs
+{
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    [BepInProcess("Windose.exe")]
+    public class NGOPlugin : BaseUnityPlugin
+    {
+        public const string pluginGuid = "needy.girl.logs";
+        public const string pluginName = "Logs";
+        public const string pluginVersion = "1.0.0.0";
+
+        public static ManualLogSource logger = new ManualLogSource(pluginGuid);
+        public static PluginInfo PInfo { get; private set; }
+        public void Awake()
+        {
+            PInfo = Info;
+
+            Logger.LogInfo("Saves actions and events in a day in the form of logs. Logs appear after the game is closed.");
+
+            Harmony harmony = new Harmony(pluginGuid);
+            harmony.PatchAll();
+        }
+
+        public void Start()
+        {
+            ///Add some code or whatever
+        }
+    }
+
+}
+
+
