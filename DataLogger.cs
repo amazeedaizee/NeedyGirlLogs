c2925454ecc1cb79228167b34ce6780c2a8c4867
amazeedaizee <amazeedaizee@WINDSTORM>
2024-01-31 01:00:47 -0500

    Made loggers

--- DataLogger.cs
+++ DataLogger.cs
@@ -1,12 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using HarmonyLib;
+using ngov3;
 
 namespace PlaythroughLogs
 {
+    [HarmonyPatch]
     internal class DataLogger
     {
+        internal static DataInfo dataOne = null;
+        internal static DataInfo dataTwo = null;
+        internal static DataInfo dataThree = null;
+
+        internal static DayInfo currentDay = new DayInfo();
+
+        public static void SetDataInfo()
+        {
+            switch (SingletonMonoBehaviour<Settings>.Instance.saveNumber)
+            {
+                case 1:
+                    SetDataInfo(dataOne);
+                    break;
+                case 2:
+                    SetDataInfo(dataTwo);
+                    break;
+                case 3:
+                    SetDataInfo(dataThree);
+                    break;
+            }
+        }
+
+        public static void SetDataInfo(DataInfo info)
+        {
+            DayInfo newDay = new DayInfo();
+            if (info == null)
+            {
+                info = new DataInfo();
+            }
+
+        }
+
+        public static void SaveDayToData()
+        {
+            switch (SingletonMonoBehaviour<Settings>.Instance.saveNumber)
+            {
+                case 1:
+                    dataOne.Days.Add(currentDay);
+                    break;
+                case 2:
+                    dataTwo.Days.Add(currentDay);
+                    break;
+                case 3:
+                    dataThree.Days.Add(currentDay);
+                    break;
+            }
+            currentDay = new DayInfo();
+        }
+
     }
 }
---
+++ EventLogger.cs
@@ -0,0 +1,42 @@
+﻿using HarmonyLib;
+using ngov3;
+using System;
+
+namespace PlaythroughLogs
+{
+    [HarmonyPatch]
+    internal class EventLogger
+    {
+        internal static bool statusApplied;
+        internal static NgoEvent upcomingEvent;
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(EventManager), "StartEvent")]
+        internal static void EventPeeker(EventManager __instance)
+        {
+            if (__instance.eventQueue.Count > 0)
+            {
+                NgoEvent ngoEvent = __instance.eventQueue.Peek();
+
+                if (ngoEvent.ToString().Contains("Action_")) { return; }
+                if (ngoEvent.ToString().Contains("DayPassing")) { return; }
+                if (ngoEvent.ToString().Contains("Separator")) { return; }
+                if (ngoEvent.ToString().Contains("TimePassing")) { return; }
+                if (ngoEvent.ToString().Contains("_Uzagarami")) { return; }
+                if (ngoEvent.ToString().Contains("_CheckBGM")) { return; }
+                upcomingEvent = ngoEvent;
+            }
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(Boot), "Awake")]
+        [HarmonyPatch(typeof(EventManager), "EndEvent", new Type[] { })]
+        internal static void ClearPeekedEvent()
+        {
+            if (upcomingEvent != null)
+            {
+                upcomingEvent = null;
+            }
+        }
+    }
+}
